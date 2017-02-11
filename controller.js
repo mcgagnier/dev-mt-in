b65ecddb46cda98e4aa2f6ae706f47179427baeb
mcgagnier <mcgagnier@yahoo.com>
2017-02-11 11:59:58 -0700

    toggle pages working

--- controller.js
+++ controller.js
@@ -1,5 +1,17 @@
 
-angular.module('social').controller('mainCtrl', function($scope, mainService) {
-    $scope.test = "WORKING";
-    
+angular.module('social').controller('mainCtrl', function($scope) {
+    // $scope.test = "WORKING";
+    var pages = ['view', 'find', 'update'];
+    $scope.view = true;
+
+    $scope.togglePage = function (clickedNavView){
+        pages.forEach(function(page){
+            if (page === clickedNavView){
+            $scope[page] = true;
+        } else {
+            $scope[page] = false;
+        }
+        })
+    }
+
 });
